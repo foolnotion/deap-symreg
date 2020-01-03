a7759abd8a7cf64c8f43b9831ce00aed0caaeb26
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-01-03 14:36:46 +0100

    Improve performance

--- symreg.py
+++ symreg.py
@@ -31,8 +31,21 @@ rows, cols = X_train.shape
 print('X_train shape: ', X_train.shape)
 print('y_train shape: ', y_train.shape)
 
+# set static height limit for all generated trees
+pset = gp.PrimitiveSet("MAIN", cols)
+pset.addPrimitive(np.add, 2, name="vadd")
+pset.addPrimitive(np.subtract, 2, name="vsub")
+pset.addPrimitive(np.multiply, 2, name="vmul")
+pset.addPrimitive(np.divide, 2, name="vdiv")
+pset.addPrimitive(np.negative, 1, name="vneg")
+pset.addPrimitive(np.cos, 1, name="vcos")
+pset.addPrimitive(np.sin, 1, name="vsin")
+pset.addPrimitive(np.exp, 1, name="vexp")
+pset.addPrimitive(np.log, 1, name="vlog")
+pset.addEphemeralConstant("rand101", lambda: np.random.uniform(-1.0, 1.0)) #may be unable to pickle...
 
-def evalSymbReg(individual, pset):
+
+def evalSymbReg(individual):
     # Transform the tree expression in a callable function
     func = gp.compile(expr=individual, pset=pset)
     
@@ -67,19 +80,6 @@ def evalSymbReg(individual, pset):
 def main():
     np.seterr(all='ignore')
     random.seed(318)
-    
-    # set static height limit for all generated trees
-    pset = gp.PrimitiveSet("MAIN", cols)
-    pset.addPrimitive(np.add, 2, name="vadd")
-    pset.addPrimitive(np.subtract, 2, name="vsub")
-    pset.addPrimitive(np.multiply, 2, name="vmul")
-    pset.addPrimitive(np.divide, 2, name="vdiv")
-    pset.addPrimitive(np.negative, 1, name="vneg")
-    pset.addPrimitive(np.cos, 1, name="vcos")
-    pset.addPrimitive(np.sin, 1, name="vsin")
-    pset.addPrimitive(np.exp, 1, name="vexp")
-    pset.addPrimitive(np.log, 1, name="vlog")
-    pset.addEphemeralConstant("rand101", lambda: np.random.uniform(-1.0, 1.0)) #may be unable to pickle...
         
     creator.create("FitnessMin", base.Fitness, weights=(1.0,))
     creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
@@ -89,21 +89,23 @@ def main():
     
     toolbox = base.Toolbox()
     from multiprocessing.pool import Pool
-    pool = Pool(6)
+    pool = Pool(24)
     toolbox.register("map", pool.map)
     
     toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=maxHeight)
     toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-    toolbox.register("evaluate", evalSymbReg, pset=pset)
+    toolbox.register("evaluate", evalSymbReg)
     toolbox.register("select", tools.selTournament, tournsize=5)
     
     # Allow for random choice between 2 set up mutators
     toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
     toolbox.register('mutUniform', gp.mutUniform,   expr=toolbox.expr_mut, pset=pset)
     toolbox.register('mutEphemeral', gp.mutEphemeral, mode='all')
+    toolbox.register('mutNodeReplacement', gp.mutNodeReplacement, pset=pset)
+    toolbox.register('mutInsert', gp.mutInsert, pset=pset)
     
-    mutOperators = [ toolbox.mutUniform, toolbox.mutEphemeral ]
+    mutOperators = [ toolbox.mutUniform, toolbox.mutEphemeral, toolbox.mutNodeReplacement, toolbox.mutInsert ]
     
     def mutOperator(*args, **kwargs):
         mut = np.random.choice(mutOperators)
