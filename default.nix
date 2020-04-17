1ab93dee681c115752ea7927f637ff9d9b85206d
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-04-17 14:50:29 +0200

    Add nix default shell file

---
+++ default.nix
@@ -0,0 +1,15 @@
+let 
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    doCheck = false;
+#    buildInputs = with pkgs; [ _python37 ];
+
+    buildInputs = with pkgs; [ (pkgs.python37.withPackages (ps: with ps; [ pip numpy pandas cython scikitlearn pyprof2calltree memory_profiler ])) ];
+    
+    shellHook = ''
+            alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
+            export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
+            unset SOURCE_DATE_EPOCH
+    '';
+  }
